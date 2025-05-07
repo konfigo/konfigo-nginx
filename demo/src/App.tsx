6a6b3a5f9e68daec448bfa79b8d4df8d00e16739
Chamuth Chamandana <chamuth@rooster.org>
2025-05-08 00:36:11 +0530

    fix: add colors

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -3,7 +3,9 @@ import "./App.css"
 function App() {
   return (
     <>
-      <h1>Hi {import.meta.env.VITE_NAME}</h1>
+      <h1 style={{ color: import.meta.env.VITE_COLOR }}>
+        Hi {import.meta.env.VITE_NAME}
+      </h1>
     </>
   )
 }
