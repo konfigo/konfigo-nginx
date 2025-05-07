9f314f2f65358f061f3d2bb56e72b5be9f58a009
Chamuth Chamandana <chamuth@rooster.org>
2025-05-07 12:05:44 +0530

    fix: name $

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -13,7 +13,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Hi ${import.meta.env.VITE_NAME}</h1>
+      <h1>Hi {import.meta.env.VITE_NAME}</h1>
     </>
   )
 }
